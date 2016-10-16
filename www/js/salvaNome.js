b4f13a14b555fa59697ba3fceb6d38fb34fc5851
hericksoncs <hericksoncs@gmail.com>
2016-10-16 17:08:26 -0200

    ttt
    te

---
+++ www/js/salvaNome.js
@@ -0,0 +1,50 @@
+		window.addEventListener("load",function(){
+			document.getElementById("bt-salve").addEventListener("click",salveName);
+			document.getElementById("bt-remove").addEventListener("click",removerName);
+			document.getElementById("bt-sair").addEventListener("click",sair);
+			document.getElementById("sucesso").style.display= "none";
+			document.getElementById("falha").style.display= "none";
+		});
+		
+		function inicializar(){
+			if(localStorage.getItem("nome")) {
+				listarNome()
+			}
+		}
+		
+		
+		function listarNome(){
+			var mensagem = "<h1> Olá " + localStorage.getItem("nome") + "</h1>";
+			
+			document.getElementById("lista").innerHTML = mensagem;
+			
+		}
+		
+		function removerName(){
+			if(localStorage.getItem("nome")){
+				localStorage.removeItem("nome");
+				document.getElementById("lista").innerHTML = '';
+				document.getElementById("nome").value = '';
+				document.getElementById("sucesso").innerHTML = 'LocalStorage removido com sucesso!';
+				document.getElementById("sucesso").style.display= "block";
+				inicializar();
+			}else{
+				document.getElementById("falha").innerHTML = 'Não há nenhuma informação no LocalStorage';
+				document.getElementById("sucesso").style.display= "none";
+				document.getElementById("falha").style.display= "block";
+			}
+						
+		}
+		
+		function salveName(){
+			var nome = document.getElementById("nome").value;
+			localStorage.setItem("nome", nome);
+			document.getElementById("sucesso").innerHTML = 'LocalStorage adicionado com sucesso!';
+			document.getElementById("sucesso").style.display= "block";
+			document.getElementById("falha").style.display= "none";
+			inicializar();
+		}
+		
+		function sair() {
+			navigator.app.exitApp();
+		}
